1b316cd88ff8c93cf5d1022af65b23284968b94f
AgusFC~ <agustinwork@outlook.com>
2023-06-04 22:49:00 -0300

    Agregado casilla de correo modificable y funcional con confirmacion con alert de js

---
+++ script.js
@@ -0,0 +1,10 @@
+function mostrar(){
+
+    swal('Correo Enviado!');
+
+
+}
+
+
+
+// Boton funcional y colores modificados pero al utilizar formsubmmit se envia a la pagina de chekeo de captach para enviar el correo, buscar solucion
